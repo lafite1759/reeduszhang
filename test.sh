0a3da7371670df9e508339949b4f7e3ab7312e07
lafite1759 <37690464+lafite1759@users.noreply.github.com>
2018-12-14 16:57:23 +0800

    Create test.sh
    add test

---
+++ test.sh
@@ -0,0 +1,7 @@
+for varible1 in {1..9}
+#for varible1 in 1 2 3 4 5
+do
+     echo "Hello, Welcome $varible1 times "
+     echo -e "\u001b[33;1mBuild system information\u001b[0m\r"
+     sleep 5
+done
